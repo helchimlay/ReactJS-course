8a80faccb11cf30b626a60a29132232ffcae8719
Jakub Olechowski <kubaole2@op.pl>
2022-08-09 21:44:05 +0200

    Added new lesson

---
+++ React_Kurs/8. Aplikacja OrderTickets.js
@@ -0,0 +1,84 @@
+const ValidationMessage = (props) => {
+  const { message } = props
+  return <p>{message}</p>
+}
+
+const FormComponent = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <input
+        id="age"
+        type="checkbox"
+        onChange={props.handleCheckboxChange}
+        checked={props.isChecked}
+      />
+      <label htmlFor="age">Mam ukończone 16 lat.</label>
+      <br />
+      <button type="submit">Kup bilet</button>
+    </form>
+  )
+}
+
+// const PositiveMessage = () => {
+//   return <p>Możesz obejrzeć film.</p>
+// }
+
+// const NegativeMessage = () => {
+//   return <p>Nie możesz obejrzeć filmu!</p>
+// }
+
+const displayMessage = (isChecked, isFormSubmited) => {
+  if (isFormSubmited) {
+    if (isChecked) {
+      return <ValidationMessage message="Możesz obejrzeć film." />
+    } else {
+      return <ValidationMessage message="Nie możesz obejrzeć filmu!" />
+    }
+  } else {
+    return null
+  }
+}
+
+/////////////////////////////////////////////////////////////////
+
+class TicketShop extends React.Component {
+  state = {
+    isChecked: false,
+    isFormSubmited: false,
+  }
+
+  handleCheckboxChange = () => {
+    this.setState({
+      isChecked: !this.state.isChecked,
+      isFormSubmited: false,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (!this.state.isFormSubmited) {
+      this.setState({
+        isFormSubmited: true,
+      })
+    }
+  }
+
+  render() {
+    const { isChecked, isFormSubmited } = this.state
+    const { handleSubmit, handleCheckboxChange } = this
+
+    return (
+      <>
+        <h1>Kub bilet na horror.</h1>
+        <FormComponent
+          handleSubmit={handleSubmit}
+          handleCheckboxChange={handleCheckboxChange}
+          isChecked={isChecked}
+        />
+        {displayMessage(isChecked, isFormSubmited)}
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<TicketShop />, document.getElementById("root"))
